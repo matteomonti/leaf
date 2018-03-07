86aae432a578aec67dfb52c69667fde0d03cba88
Matteo Monti <matteo.monti@rain.vg>
2018-03-07 18:18:08 +0100

    Update main
    The std priority_queue seems to be more performing
    than drop's heap.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <thread>
 
-#include "drop/data/heap.hpp"
-
-using namespace drop;
+#include <queue>
 
 int main()
 {
-    heap <int> my_heap;
+    std :: priority_queue <int> my_queue;
 
     for(int i = 0; i < 100000000; i++)
     {
         if(i % 1000000 == 0)
             std :: cout << i << std :: endl;
 
-        my_heap.push(rand());
+        my_queue.push(rand());
     }
 
     int total = 0;
 
-    for(int i = 0; i < 100000000; i++)
+    for(size_t i = 0; i < 100000000; i++)
     {
-        int value = my_heap.pop();
+        int value = my_queue.top();
+        my_queue.pop();
 
         if(i % 1000000 == 0)
         {
