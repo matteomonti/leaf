b42c43752f30d2a5c3e963b0d96d310dab948771
Matteo Monti <matteo.monti@rain.vg>
2018-03-08 12:46:30 +0100

    Delete data/heap
    It's pointless to waste time re-implementing an
    already optimized data structure.

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,6 @@
 #include <iostream>
 #include <thread>
 
-#include <queue>
-
 int main()
 {
-    std :: priority_queue <int> my_queue;
-
-    for(int i = 0; i < 100000000; i++)
-    {
-        if(i % 1000000 == 0)
-            std :: cout << i << std :: endl;
-
-        my_queue.push(rand());
-    }
-
-    int total = 0;
-
-    for(size_t i = 0; i < 100000000; i++)
-    {
-        int value = my_queue.top();
-        my_queue.pop();
-
-        if(i % 1000000 == 0)
-        {
-            std :: cout << i << std :: endl;
-            total += value;
-        }
-    }
-
-    std :: cout << total << std :: endl;
 }
