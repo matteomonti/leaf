7dd3d4f9850949f361d0ecad212dea0f9ba55fe6
Matteo Monti <matteo.monti@rain.vg>
2018-03-13 00:52:40 +0100

    Delete network/pool
    Pools need to be done properly. Better start from
    scratch than trying to bugfix all the segmentation
    faults that the previous implementation produced.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
 #include <thread>
 
-#include "drop/network/connectors/tcp.h"
-#include "drop/network/pool.hpp"
-
 using namespace drop;
 
 int main()
 {
-    pool my_pool;
-
-    connection my_connection = connectors :: tcp :: sync :: connect({"127.0.0.1", 1234});
-
-    pool :: connection my_pool_connection = my_pool.bind(my_connection);
-    my_pool_connection.send("Hello World!").then([&]()
-    {
-        std :: cout << "Message sent!" << std :: endl;
-        return my_pool_connection.receive();
-    }).then([](const buffer & message)
-    {
-        std :: cout << "Message received: " << message << std :: endl;
-    });
-
-    sleep(1_h);
 }
