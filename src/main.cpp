2aebc7144402b7bc4a4d73bbb0a5985326c3daca
Matteo Monti <matteo.monti@rain.vg>
2018-04-20 14:42:17 +0200

    Add poseidon/benchmark/coordinator
    Basic implementation of a coordinator server, needed
    to exchange the initial view.

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,61 @@
 #include <iostream>
-#include <fstream>
 
-#include "poseidon/poseidon/poseidon.hpp"
-#include "drop/network/connectors/tcp.h"
-#include "drop/network/pool.hpp"
-#include "drop/chrono/crontab.h"
-#include "vine/dialers/local.h"
-#include "vine/network/multiplexer.hpp"
+#include "poseidon/benchmark/coordinator.h"
+#include "drop/crypto/signature.hpp"
 
 using namespace drop;
 using namespace vine;
 using namespace poseidon;
 
-static constexpr size_t nodes = 256;
-
-std :: array <identifier, brahms :: settings :: view :: size> view(std :: array <signer, nodes> & signers, size_t exclude)
+struct ports
 {
-    std :: array <identifier, brahms :: settings :: view :: size> sample;
+    static constexpr uint16_t coordinator = 7777;
+    static constexpr uint16_t directory = 7778;
+};
 
-    for(size_t i = 0; i < brahms :: settings :: view :: size; i++)
-    {
-        size_t pick = rand() % nodes;
-
-        while(pick == exclude)
-            pick = rand() % nodes;
-
-        sample[i] = signers[pick].publickey();
-    }
-
-    return sample;
-}
-
-void seppuku()
+int main(int argc, char ** args)
 {
-    while(true)
+    if(argc < 2)
     {
-        char buffer[1024];
-        std :: cin >> buffer;
-
-        if(strcmp("seppuku", buffer) == 0)
-            *((int *) nullptr) = 99;
-    }
-}
-
-int main()
-{
-    std :: thread seppuku_thread(seppuku);
-    seppuku_thread.detach();
-
-    std :: ofstream mute;
-    mute.open("/dev/null", std :: ios :: out);
-
-    connectors :: tcp :: async connector;
-    pool pools[8];
-    crontab crontabs[8];
-
-    dialers :: local :: server server;
-
-    std :: array <signer, nodes> signers;
-    std :: array <multiplexer <dialers :: local :: client, 3> *, nodes> dialers;
-    std :: array <class poseidon *, nodes> clients;
-
-    std :: cout << "Creating nodes" << std :: endl;
-
-    for(size_t i = 0; i < nodes; i++)
-    {
-        auto view = :: view(signers, i);
-        dialers[i] = new multiplexer <dialers :: local :: client, 3> (server, signers[i], pools[i % 8]);
-        clients[i] = new class poseidon(signers[i], view, *(dialers[i]), pools[i % 8], crontabs[i % 8], ((i == 0 && false) ? std :: cout : mute));
+        std :: cout << "Please select a role for the node: master or peer." << std :: endl;
+        return -1;
     }
 
-    std :: cout << "Registering handlers" << std :: endl;
-
-    clients[0]->on <events :: gossip> ([&](const statement & statement)
+    if(!strcmp(args[1], "master"))
     {
-        std :: cout << timestamp(now) << " Statement gossiped: " << statement.identifier() << " / " << statement.sequence() << ": " << statement.value() << std :: endl;
-    });
+        if(argc < 3)
+        {
+            std :: cout << "Please insert the number of nodes to coordinate." << std :: endl;
+            return -1;
+        }
 
-    clients[0]->on <events :: accept> ([&](const statement & statement)
-    {
-        std :: cout << timestamp(now) << " Statement accepted: " << statement.identifier() << " / " << statement.sequence() << ": " << statement.value() << std :: endl;
-    });
+        size_t nodes = atoi(args[2]);
 
-    std :: cout << "Starting nodes" << std :: endl;
+        if(nodes == 0)
+        {
+            std :: cout << "The network cannot have zero nodes." << std :: endl;
+            return -1;
+        }
 
-    for(size_t i = 0; i < nodes; i++)
-    {
-        std :: cout << "Starting node " << i << std :: endl;
-        clients[i]->start();
-        sleep(20_ms);
+        coordinator coordinator(ports :: coordinator, nodes);
+        sleep(10_h);
     }
-
-    std :: cout << "Started" << std :: endl;
-
-    sleep(1_m);
-
-    for(uint64_t i = 0;; i++)
+    else if(!strcmp(args[1], "peer"))
     {
-        char message[1024];
-        sprintf(message, "Message number %llu", i);
+        if(argc < 3)
+        {
+            std :: cout << "Please insert the IP address of the master." << std :: endl;
+            return -1;
+        }
 
-        std :: cout << "Seeding " << message << std :: endl;
-        clients[44]->publish(i, message);
+        signer signer;
 
-        sleep(1_s);
+        address coordaddr(args[2], ports :: coordinator);
+        address diraddr(args[2], ports :: directory);
+
+        std :: array <identifier, brahms :: settings :: view :: size> view = coordinator :: await(coordaddr, signer.publickey());
+
+        for(size_t i = 0; i < brahms :: settings :: view :: size; i++)
+            std :: cout << view[i] << std :: endl;
     }
-
-    sleep(10_h);
 }
---
+++ src/poseidon/benchmark/coordinator.h
@@ -0,0 +1,78 @@
+// Forward declarations
+
+namespace poseidon
+{
+    class coordinator;
+};
+
+#if !defined(__forward__) && !defined(__poseidon__benchmark__coordinator__h)
+#define __poseidon__benchmark__coordinator__h
+
+// Libraries
+
+#include <unordered_set>
+#include <vector>
+#include <mutex>
+
+// Includes
+
+#include "drop/network/acceptors/tcp.h"
+#include "drop/network/connectors/tcp.h"
+#include "drop/network/pool.hpp"
+#include "drop/crypto/shorthash.hpp"
+#include "vine/network/dial.h"
+#include "poseidon/brahms/brahms.h"
+
+namespace poseidon
+{
+    using namespace drop;
+    using namespace vine;
+
+    class coordinator
+    {
+        // Settings
+
+        struct settings
+        {
+            struct intervals
+            {
+                static constexpr interval poll = 1_s;
+            };
+        };
+
+        // Members
+
+        size_t _nodes;
+
+        struct
+        {
+            std :: unordered_set <identifier, shorthash> set;
+            std :: vector <identifier> vector;
+        } _identifiers;
+
+        acceptors :: tcp :: async _acceptor;
+        pool _pool;
+
+        std :: mutex _mutex;
+
+    public:
+
+        // Constructors
+
+        coordinator(const uint16_t &, const size_t &);
+
+    private:
+
+        // Private methods
+
+        promise <void> serve(pool :: connection);
+
+    public:
+
+        // Static methods
+
+        static std :: array <identifier, brahms :: settings :: view :: size> await(const address &, const identifier &);
+    };
+};
+
+#endif
