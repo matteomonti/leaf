f5940bebf5771757dd244e499e38bf16cb1e9452
Matteo Monti <matteo.monti@rain.vg>
2018-04-25 17:03:39 +0200

    Update main
    Full-fledged experiment with accept. Completely
    untested.

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,287 @@
 #include <iostream>
 
+#include "drop/distributed/gossiper.hpp"
 #include "drop/distributed/publisher.hpp"
-#include "drop/network/sockets/local.hpp"
+#include "poseidon/benchmark/coordinator.h"
+#include "drop/crypto/signature.hpp"
+#include "vine/dialers/directory.hpp"
+#include "vine/network/multiplexer.hpp"
 
 using namespace drop;
+using namespace vine;
+using namespace poseidon;
 
-int main()
+struct ports
 {
-    pool pool;
-    crontab crontab;
+    static constexpr uint16_t coordinator = 7777;
+    static constexpr uint16_t directory = 7778;
+};
 
-    publisher <uint64_t, buffer> mypublisher(crontab);
+struct intervals
+{
+    static constexpr interval initial = 10_s;
+    static constexpr interval final = 30_s;
 
-    [&]() -> promise <void>
+    static constexpr interval total = 10_m;
+    static constexpr interval publish = 0.1_s;
+    static constexpr interval change = 1_m;
+};
+
+// Statement
+
+struct statement
+{
+    // Public members
+
+    uint64_t id;
+    buffer message;
+
+    // Methods
+
+    template <typename vtype> void accept(bytewise :: reader <vtype> & reader) const
     {
-        while(true)
-        {
-            co_await crontab.wait(1_s);
-            mypublisher.publish(44, "Hello World!");
-        }
-    }();
-
-    [&]() -> promise <void>
-    {
-        while(true)
-        {
-            co_await crontab.wait(0.5_s);
-            mypublisher.publish(45, "World 45 is better!");
-        }
-    }();
-
-    [&]() -> promise <void>
-    {
-        while(true)
-        {
-            co_await crontab.wait(0.1_s);
-            mypublisher.publish(46, "I'm a crazy spammer!");
-        }
-    }();
-
-    mypublisher.on <publisher <uint64_t, buffer> :: archive> ([](const auto & topic, const auto & archive)
-    {
-        std :: cout << "[archive] Request for archive on topic " << topic << std :: endl;
-
-        archive << "Hello from the Archive!";
-        archive << "Super hello from the Archive!";
-        archive << "Duper hello from the Archive!";
-    });
-
-    sockets :: socketpair socketpair;
-    mypublisher.serve(pool.bind(socketpair.beta));
-
-    sockets :: socketpair othersocketpair;
-    mypublisher.serve(pool.bind(othersocketpair.beta));
-
-    subscriber <uint64_t, buffer> myothersubscriber(pool.bind(othersocketpair.alpha), crontab);
-
-    size_t n = 0;
-    myothersubscriber.on <buffer> ([&](const uint64_t & topic, const buffer & payload)
-    {
-        std :: cout << "Other subscriber receives: " << topic << ", " << payload << std :: endl;
-        if(n++ >= 100)
-            myothersubscriber.unsubscribe(topic);
-    });
-
-    myothersubscriber.on <drop :: close> ([]()
-    {
-        std :: cout << "Other subscriber closing" << std :: endl;
-    });
-
-    myothersubscriber.start();
-
-    myothersubscriber.subscribe(46);
-
-    {
-        subscriber <uint64_t, buffer> mysubscriber(pool.bind(socketpair.alpha), crontab);
-
-        mysubscriber.on <buffer> ([](const uint64_t & topic, const buffer & payload)
-        {
-            std :: cout << "Subscriber receives: " << topic << ", " << payload << std :: endl;
-        });
-
-        mysubscriber.on <drop :: close> ([]()
-        {
-            std :: cout << "Subscriber closing" << std :: endl;
-        });
-
-        mysubscriber.start();
-
-        mysubscriber.subscribe(44);
-        sleep(4_s);
-        mysubscriber.unsubscribe(44);
-        sleep(4_s);
-        mysubscriber.once(46);
-        sleep(4_s);
-        mysubscriber.subscribe(45);
-        sleep(4_s);
-        mysubscriber.subscribe(44);
-        sleep(10_s);
+        reader << (this->id);
+        reader << (this->message);
     }
 
-    sleep(10_h);
+    template <typename vtype> void accept(bytewise :: writer <vtype> & writer)
+    {
+        writer >> (this->id);
+        writer >> (this->message);
+    }
+
+    // Operators
+
+    bool operator == (const statement & rho) const
+    {
+        return (this->id == rho.id) && (this->message == rho.message);
+    }
+
+    bool operator != (const statement & rho) const
+    {
+        return !((*this) == rho);
+    }
+};
+
+int main(int argc, char ** args)
+{
+    if(argc < 2)
+    {
+        std :: cout << "Please select a role for the node: master or peer." << std :: endl;
+        return -1;
+    }
+
+    if(!strcmp(args[1], "master"))
+    {
+        if(argc < 3)
+        {
+            std :: cout << "Please insert the number of nodes to coordinate." << std :: endl;
+            return -1;
+        }
+
+        size_t nodes = atoi(args[2]);
+
+        if(nodes == 0)
+        {
+            std :: cout << "The network cannot have zero nodes." << std :: endl;
+            return -1;
+        }
+
+        coordinator coordinator(ports :: coordinator, nodes);
+        dialers :: directory :: server directory(ports :: directory);
+        sleep(10_h);
+    }
+    else if(!strcmp(args[1], "peer"))
+    {
+        if(argc < 3)
+        {
+            std :: cout << "Please insert the IP address of the master." << std :: endl;
+            return -1;
+        }
+
+        if(argc < 4)
+        {
+            std :: cout << "Please insert the ID of the current instance." << std :: endl;
+            return -1;
+        }
+
+        size_t instanceid = atoi(args[3]);
+
+        address coordaddr(args[2], ports :: coordinator);
+        address diraddr(args[2], ports :: directory);
+
+        // Coordinator
+
+        signer signer;
+        std :: array <identifier, coordinator :: settings :: view :: size> view = coordinator :: await(coordaddr, signer.publickey());
+        std :: array <identifier, coordinator :: settings :: view :: size> sample = coordinator :: await(coordaddr, signer.publickey());
+
+        // Various declarations
+
+        connectors :: tcp :: async connector;
+        pool pool;
+        crontab crontab;
+
+        // Publisher
+
+        std :: mutex mutex;
+
+        std :: unordered_map <uint64_t, buffer> statements;
+        std :: unordered_map <uint64_t, size_t> votes;
+
+        publisher <uint64_t, buffer> ppublisher(crontab);
+
+        ppublisher.on <publisher <uint64_t, buffer> :: archive> ([&](const auto & id, const auto & archive)
+        {
+            mutex.lock();
+
+            try
+            {
+                buffer message = statements.at(id);
+                archive << message;
+            }
+            catch(...)
+            {
+            }
+
+            mutex.unlock();
+        });
+
+        subscriber <uint64_t, buffer> * subscribers[coordinator :: settings :: view :: size]{};
+
+        // Gossiper
+
+        gossiper <statement> gossiper(crontab);
+
+        gossiper.on <statement> ([&](const auto & id, const statement & statement)
+        {
+            std :: cout << (uint64_t) timestamp(now) << " gossip " << statement.id << ": " << statement.message << std :: endl;
+
+            mutex.lock();
+            statements[statement.id] = statement.message;
+
+            for(size_t i = 0; i < coordinator :: settings :: view :: size; i++)
+                if(subscribers[i])
+                    subscribers[i]->once(statement.id);
+
+            mutex.unlock();
+
+            ppublisher.publish(statement.id, statement.message);
+        });
+
+        // Dialer
+
+        multiplexer <dialers :: directory :: client, 2> dialer(diraddr, signer, connector, pool, crontab);
+
+        dialer.on <0> ([&](const dial & dial)
+        {
+            gossiper.serve(pool.bind(dial), signer.publickey() < dial.identifier());
+        });
+
+        dialer.on <1> ([&](const dial & dial)
+        {
+            ppublisher.serve(pool.bind(dial));
+        });
+
+        // Seeding
+
+        timestamp start = now;
+
+        if(instanceid == 0)
+        {
+            [&]() -> promise <void>
+            {
+                for(uint64_t round = 0; (timestamp(now) - start) < intervals :: total; round++)
+                {
+                    std :: cout << (uint64_t) timestamp(now) << " seed " << round << std :: endl;
+
+                    char message[1024];
+                    sprintf(message, "Message number %llu", round);
+                    gossiper.publish({.id = round, .message = message});
+
+                    co_await crontab.wait(intervals :: publish);
+                }
+            }();
+        }
+
+        sleep(intervals :: initial);
+
+        // Experiment
+
+        while(true)
+        {
+            if(timestamp(now) - start > intervals :: total + intervals :: final)
+                break;
+
+            mutex.lock();
+            for(size_t i = 0; i < coordinator :: settings :: view :: size; i++)
+                if(subscribers[i])
+                {
+                    delete subscribers[i];
+                    subscribers[i] = nullptr;
+                }
+            mutex.unlock();
+
+            for(size_t i = 0; i < coordinator :: settings :: view :: size; i++)
+            {
+                size_t index = i;
+                identifier identifier = sample[index];
+
+                dialer.connect <1> (identifier).then([&](const dial & dial)
+                {
+                    mutex.lock();
+
+                    subscribers[index] = new subscriber <uint64_t, buffer> (pool.bind(dial), crontab);
+                    subscribers[index]->on <buffer> ([&](const uint64_t & id, const buffer & message)
+                    {
+                        mutex.lock();
+                        try
+                        {
+                            votes.at(id)++;
+                        }
+                        catch(...)
+                        {
+                            votes[id] = 1;
+                        }
+
+                        if(votes[id] == coordinator :: settings :: view :: size)
+                        {
+                            std :: cout << (uint64_t) timestamp(now) << " accept " << id << ": " << message << std :: endl;
+                            votes.erase(id);
+                        }
+
+                        mutex.unlock();
+                    });
+
+                    for(const auto & vote : votes)
+                        subscribers[index]->once(vote.first);
+
+                    subscribers[index]->start();
+
+                    mutex.unlock();
+                }).except([=](const std :: exception_ptr & exception)
+                {
+                    std :: cout << "Failed to establish (sample) connection to " << identifier << std :: endl;
+                });
+            }
+
+            mutex.lock();
+            votes.clear();
+            mutex.unlock();
+
+            for(const identifier & identifier : view)
+                dialer.connect <0> (identifier).then([&](const dial & dial)
+                {
+                    gossiper.serve(pool.bind(dial), signer.publickey() < dial.identifier()).until(timestamp(now) + interval :: random(intervals :: change));
+                }).except([=](const std :: exception_ptr & exception)
+                {
+                    std :: cout << "Failed to establish (view) connection to " << identifier << std :: endl;
+                });
+
+            sleep(interval :: random(intervals :: change));
+
+            view = coordinator :: await(coordaddr, signer.publickey());
+            sample = coordinator :: await(coordaddr, signer.publickey());
+        }
+    }
 }
