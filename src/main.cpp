f5f7e39fba8142f4c53832db6f52ee671d094b14
Matteo Monti <matteo.monti@rain.vg>
2018-04-27 19:44:38 +0200

    Update main
    Adds wait before starting peer.

--- src/main.cpp
+++ src/main.cpp
@@ -75,8 +75,10 @@ int main(int argc, char ** args)
         std :: vector <identifier> sample = coordinator :: await(coordaddr, signer.publickey(), sizes :: sample);
 
         staticsample :: peer peer(instanceid, signer, rate, view, sample, diraddr);
+
+        wait(10_s);
         peer.start();
-        
+
         while(true)
             sleep(10_h);
     }
