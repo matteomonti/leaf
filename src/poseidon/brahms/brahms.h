2ecd81214cae8187ef2943381eac7c0718618431
Matteo Monti <matteo.monti@rain.vg>
2018-04-28 16:38:51 +0200

    Update poseidon/brahms/brahms
    More basic scaffolding. Pausing to implement sending
    and receiving std :: arrays.

--- src/poseidon/brahms/brahms.h
+++ src/poseidon/brahms/brahms.h
@@ -12,16 +12,37 @@ namespace poseidon
 
 #include "poseidon/settings.h"
 #include "drop/network/pool.hpp"
+#include "drop/chrono/crontab.h"
 #include "drop/async/collector.hpp"
+
 #include "vine/network/dial.h"
+#include "sampler.h"
 
 namespace poseidon
 {
     using namespace drop;
     using namespace vine;
 
+    // Typedefs
+
+    typedef std :: array <identifier, settings :: view :: size> view;
+    typedef std :: array <sampler, settings :: sample :: size> sample;
+
+    // Classes
+
     class brahms
     {
+    private:
+
+        // Members
+
+        view _view;
+        sample _sample;
+
+        pool & _pool;
+        crontab & _crontab;
+
+        std :: mutex _mutex;
     };
 };
 
--- src/poseidon/settings.h
+++ src/poseidon/settings.h
@@ -8,11 +8,23 @@ namespace poseidon
 #if !defined(__forward__) && !defined(__poseidon__settings__h)
 #define __poseidon__settings__h
 
+// Includes
+
+#include <stddef.h>
+
 namespace poseidon
 {
     struct settings
     {
+        struct sample
+        {
+            static constexpr size_t size = 32;
+        };
 
+        struct view
+        {
+            static constexpr size_t size = 16;
+        };
     };
 };
 
